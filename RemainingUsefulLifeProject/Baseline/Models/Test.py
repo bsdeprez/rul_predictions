6abe001962073433741965ee0d851555598599a4
Brecht <Brecht.Deprez@ugent.be>
2021-05-15 17:08:52 +0200

    Lowered the learning rate

--- RemainingUsefulLifeProject/Baseline/Models/Test.py
+++ RemainingUsefulLifeProject/Baseline/Models/Test.py
@@ -6,28 +6,25 @@ from RemainingUsefulLifeProject.MAML.Models.maml import loss_function
 
 
 def copy_model(model):
-    copy = FFNModel(model.input_size)
+    copy = MyModel(model.input_size)
     copy.model.set_weights(model.model.get_weights())
     return copy
 
 
-class FFNModel:
+class MyModel:
 
-    def __init__(self, input_size, lr=0.02):
+    def __init__(self, input_size, lr=0.00001):
         self.input_size = input_size
         self.model = Sequential()
-        self.model.add(Dense(24, input_dim=self.input_size, activation='relu'))
-        self.model.add(Dense(5))
-        self.model.add(Dense(3))
+        self.model.add(Dense(200, input_dim=self.input_size, activation='relu'))
+        self.model.add(Dense(250))
+        self.model.add(Dense(200))
         self.model.add(Dense(1))
-        optimizer = keras.optimizers.Adam(learning_rate=lr)
+        optimizer = keras.optimizers.RMSprop(learning_rate=lr)
         self.model.compile(loss='mean_squared_error', optimizer=optimizer)
 
     def train(self, x, y, epochs):
-        return self.model.fit(x, y, epochs=epochs, validation_split=0.1)
-        """, callbacks=[
-            keras.callbacks.EarlyStopping(patience=10, monitor='val_loss', restore_best_weights=True)
-        ]) """
+        return self.model.fit(x, y, epochs=epochs, validation_split=0.1, batch_size=150)
 
     def predict(self, x):
         return self.model.predict(x)
--- RemainingUsefulLifeProject/main.py
+++ RemainingUsefulLifeProject/main.py
@@ -1,5 +1,27 @@
+from RemainingUsefulLifeProject.Baseline.Models.FNNs.Baseline import FFNModel
+from RemainingUsefulLifeProject.Baseline.Models.Test import MyModel
 from RemainingUsefulLifeProject.Data.cmapss_dao import DataObject
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+def get_data(train_df, test_df, dao):
+    train_x, train_y = train_df[dao.sensor_names].values, train_df['RUL'].values
+    test_df = test_df.groupby(by='unit_nr').last().reset_index()
+    test_x, test_y = test_df[dao.sensor_names].values, test_df['RUL'].values
+    if len(test_y) > 100:
+        index = np.random.choice(len(test_y), 100, replace=False)
+        test_x, test_y = test_x[index], test_y[index]
+    return train_x, train_y, test_x, test_y
+
 
 filepath = "../Data/CMAPSSData/"
-
-FD002 = DataObject('FD002', filepath=filepath)
+FD001 = DataObject('FD001', filepath=filepath)
+train, test = FD001.datasets[1]
+x_train, y_train, _, _ = get_data(train, test, FD001)
+model = MyModel(len(FD001.sensor_names))
+history = model.train(x_train, y_train, epochs=100)
+plt.plot(history.history['loss'][1:], label='loss')
+plt.plot(history.history['val_loss'][1:], label='val_loss')
+plt.legend(loc='upper left')
+plt.show()
